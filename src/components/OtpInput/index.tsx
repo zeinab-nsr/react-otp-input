5b53ed963743460d7cd33484a3658da575c46c6c
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2022-11-25 12:34:18 +0330

    STYLE: basic inputs styles added

--- src/components/OtpInput/index.tsx
+++ src/components/OtpInput/index.tsx
@@ -10,10 +10,10 @@ export const OtpInput: React.FC<Props> = ({ inputCount, inputLength }) => {
 
   return (
     <form className="otp-input__form">
-      <input type="text" className="otp-input__input" maxLength={inputLength} />
-      <input type="text" className="otp-input__input" maxLength={inputLength} />
-      <input type="text" className="otp-input__input" maxLength={inputLength} />
-      <input type="text" className="otp-input__input" maxLength={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
     </form>
   )
 }
