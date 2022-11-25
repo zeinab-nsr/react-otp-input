0d65a2e099fb45b4e1815ff50edde69bef484ec7
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2022-11-25 17:51:16 +0330

    FEATURE: focus on next input added

--- src/components/OtpInput/index.tsx
+++ src/components/OtpInput/index.tsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-export interface Props {
+interface Props {
   inputCount: number,
   inputLength: number,
   label?: string,
 }
-
 export const OtpInput: React.FC<Props> = ({ inputCount, inputLength, label }) => {
 
   const goToNextInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-
+    if(e.target.value.length === inputLength) {
+      const nextInput = e.target.nextElementSibling as HTMLElement;
+      nextInput?.focus();
+    }
   }
 
   return (
     <form className="otp-input__form">
-      {[...Array(inputCount)].map(()=>(
-        <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} onChange={goToNextInput} />
+      {[...Array(inputCount)].map((i, idx)=>(
+        <input 
+          key={idx}
+          type="text" 
+          className="otp-input__input" 
+          //ref={React.createRef()} 
+          maxLength={inputLength} 
+          size={inputLength} 
+          onChange={goToNextInput} 
+          //autoFocus={false}
+        />
       ))}
     </form>
   )
