1ca6eb1922a8092cffd1989880aee9f22862bb07
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2022-11-25 11:26:20 +0330

    FEATURE: static inputs added

---
+++ src/components/OtpInput/index.tsx
@@ -0,0 +1,19 @@
+import React, { ClipboardEvent, RefObject } from "react";
+
+export interface Props {
+  inputCount: number,
+  inputLength: number,
+
+}
+
+export const OtpInput: React.FC<Props> = ({ inputCount, inputLength }) => {
+
+  return (
+    <form className="otp-input__form">
+      <input type="text" className="otp-input__input" maxLength={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} />
+      <input type="text" className="otp-input__input" maxLength={inputLength} />
+    </form>
+  )
+}
