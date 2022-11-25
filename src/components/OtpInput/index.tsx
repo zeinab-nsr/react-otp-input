e83ab70b6166095794f481b70f0b875b0425f155
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2022-11-25 12:58:40 +0330

    CHORE: generate inputs by a loop an input count

--- src/components/OtpInput/index.tsx
+++ src/components/OtpInput/index.tsx
@@ -1,19 +1,22 @@
-import React, { ClipboardEvent, RefObject } from "react";
+import React from "react";
 
 export interface Props {
   inputCount: number,
   inputLength: number,
-
+  label?: string,
 }
 
-export const OtpInput: React.FC<Props> = ({ inputCount, inputLength }) => {
+export const OtpInput: React.FC<Props> = ({ inputCount, inputLength, label }) => {
+
+  const goToNextInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  }
 
   return (
     <form className="otp-input__form">
-      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
-      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
-      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
-      <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} />
+      {[...Array(inputCount)].map(()=>(
+        <input type="text" className="otp-input__input" maxLength={inputLength} size={inputLength} onChange={goToNextInput} />
+      ))}
     </form>
   )
 }
