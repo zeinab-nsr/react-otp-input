aa2c378b3d80e0b780ac2cf07faec2412392ca22
Zeinab Nasiri <zeinabb@Zeinabs-MacBook-Pro.local>
2022-11-30 08:52:25 +0330

    FEATURE: add get input total value prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 import { OtpInput } from './components/OtpInput';
 import './App.scss';
 
 function App() {
+  const [totalValue, setTotalValue] = useState('')
+
   return (
-    <div className="container">
-      <OtpInput label="Enter code:" inputCount={4} inputLength={1} separator="-" isNumeric />
-    </div>
+    <section className="container">
+      <OtpInput label="Enter code:" inputCount={4} inputLength={1} separator="-" isNumeric getValue={(value: string) => setTotalValue(value)} />
+      <span className="result">{totalValue}</span>
+    </section>
   );
 }
 
--- src/components/OtpInput/index.tsx
+++ src/components/OtpInput/index.tsx
@@ -3,12 +3,13 @@ import React, { useState, useRef, useEffect } from "react";
 interface Props {
   inputCount: number,
   inputLength: number,
+  getValue: (value: string) => void;
   label?: string,
   separator?: string,
   isNumeric?: boolean;
 }
 
-export const OtpInput: React.FC<Props> = ({ inputCount, inputLength, label, separator, isNumeric }) => {
+export const OtpInput: React.FC<Props> = ({ inputCount, inputLength, label, separator, isNumeric, getValue }) => {
   const [otp, setOtp] = useState<string[]>(new Array(inputCount).fill(''));
   const [activeInputIndex, setActiveInputIndex] = useState(0);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -43,33 +44,41 @@ export const OtpInput: React.FC<Props> = ({ inputCount, inputLength, label, sepa
       }
   }
 
+  const getTotalNumber = () => {
+    const totalValue = otp.join('');
+    getValue(totalValue);
+  }
+
   useEffect(() => {
     inputRef.current?.focus();
   }, [activeInputIndex])
 
   return (
     <form className="otp-input__form">
-      {label && <span className="otp-input__label">
-        {label}
-      </span>}
-      {[...Array(inputCount)].map((_, index)=>(
-        <React.Fragment key={index}>
-        <input 
-          type="text" 
-          ref={index === activeInputIndex ? inputRef : null}
-          value={otp[index]}
-          className="otp-input__input" 
-          maxLength={inputLength} 
-          size={inputLength}
-          onChange={(e) => handleChange(e, index)}
-          onKeyUp={(e) => handleKeyUp(e, index)}
-          onKeyDown={(e) => handleKeyDown(e, index)}
-        />
-        {
-          (index < inputCount - 1) && <div className="otp-input__separator">{separator}</div>
-        }
-        </React.Fragment>
-      ))}
+      <div className="otp-input__wrapper">
+        {label && <span className="otp-input__label">
+          {label}
+        </span>}
+        {[...Array(inputCount)].map((_, index)=>(
+          <React.Fragment key={index}>
+          <input 
+            type="text" 
+            className="otp-input__input" 
+            ref={index === activeInputIndex ? inputRef : null}
+            value={otp[index]}
+            maxLength={inputLength} 
+            size={inputLength}
+            onChange={(e) => handleChange(e, index)}
+            onKeyUp={(e) => handleKeyUp(e, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+          />
+          {
+            (index < inputCount - 1) && <div className="otp-input__separator">{separator}</div>
+          }
+          </React.Fragment>
+        ))}
+      </div>
+      <input type="button" className="otp-input__button" value="Get total value!" onClick={getTotalNumber} />
     </form>
   )
 }
